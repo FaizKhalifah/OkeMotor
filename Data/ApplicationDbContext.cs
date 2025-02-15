b2ed6677860fd6463d8d8f04e107901e7caf889e
Faiz Khalifah <faiz_doc@outlook.com>
2025-02-15 20:51:56 +0700

    feat:identity migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OkeMotor.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace OkeMotor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Tambahkan peran default saat database dibuat
+            builder.Entity<ApplicationUser>()
+                .HasIndex(u => u.Role)
+                .IsUnique(false);
+        }
+    }
+}
---
+++ Models/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OkeMotor.Models.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Role { get; set; }
+    }
+}
